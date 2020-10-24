ffe49d0038c35e736926c64cea023505d7406877
Andrey <andrewgurew123@gmail.com>
2020-10-25 02:04:11 +1000

    Сконфигурировано позиционирование объектов

--- MarsIOS/MarsIOS/Screens/AR/View/ARViewController.swift
+++ MarsIOS/MarsIOS/Screens/AR/View/ARViewController.swift
@@ -16,6 +16,11 @@ class ARViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     @IBOutlet var arView: VirtualObjectARView!
     @IBOutlet weak var addButton: UIButton!
     
+    var screenCenter: CGPoint {
+        let bounds = arView.bounds
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    
     let updateQueue = DispatchQueue(label: "com.example.apple-samplecode.arkitexample.serialSceneKitQueue")
     
     private var session: ARSession {
@@ -30,14 +35,73 @@ class ARViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     }
         
     @IBAction func onAddTap(_ sender: Any) {
+        let modelsURL = Bundle.main.url(forResource: "Models.scnassets", withExtension: nil)!
+
+        let fileEnumerator = FileManager().enumerator(at: modelsURL, includingPropertiesForKeys: [])!
+
+        let a:[VirtualObject] = fileEnumerator.compactMap { element in
+            let url = element as! URL
+
+            guard url.pathExtension == "scn" && !url.path.contains("lighting") else { return nil }
+
+            return VirtualObject(url: url)
+        }
         
+        loadVirtualObject(a[0], loadedHandler: { [unowned self] loadedObject in
+            self.arView.prepare([loadedObject], completionHandler: { _ in
+                DispatchQueue.main.async {
+                    guard let cameraTransform = arView.session.currentFrame?.camera.transform,
+                        let result = arView.smartHitTest(
+                            screenCenter,
+                            infinitePlane: false,
+                            objectPosition: loadedObject.simdWorldPosition,
+                            allowedAlignments: loadedObject.allowedAlignments)
+                        else { return }
+                    
+                    let planeAlignment: ARPlaneAnchor.Alignment
+                    if let planeAnchor = result.anchor as? ARPlaneAnchor {
+                        planeAlignment = planeAnchor.alignment
+                    } else if result.type == .estimatedHorizontalPlane {
+                        planeAlignment = .horizontal
+                    } else if result.type == .estimatedVerticalPlane {
+                        planeAlignment = .vertical
+                    } else {
+                        return
+                    }
+
+                    /*
+                     Plane hit test results are generally smooth. If we did *not* hit a plane,
+                     smooth the movement to prevent large jumps.
+                     */
+                    let transform = result.worldTransform
+                    let isOnPlane = result.anchor is ARPlaneAnchor
+                    loadedObject.setTransform(transform,
+                                        relativeTo: cameraTransform,
+                                        smoothMovement: !isOnPlane,
+                                        alignment: planeAlignment,
+                                        allowAnimation: false)
+                    
+                    self.arView.scene.rootNode.addChildNode(loadedObject)
+                    self.arView.addOrUpdateAnchor(for: loadedObject)
+                    
+                    loadedObject.isHidden = false
+                }
+            })
+        })
     }
     
+    func loadVirtualObject(_ object: VirtualObject, loadedHandler: @escaping (VirtualObject) -> Void) {
+        
+        DispatchQueue.global(qos: .background).async {
+            object.reset()
+            object.load()
+
+            loadedHandler(object)
+        }
+    }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-
-        session.pause()
     }
     
     func setupCamera() {
@@ -45,10 +109,6 @@ class ARViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
             fatalError("Expected a valid `pointOfView` from the scene.")
         }
 
-        /*
-         Enable HDR camera settings for the most realistic appearance
-         with environmental lighting and physically based materials.
-         */
         camera.wantsHDR = true
         camera.exposureOffset = -1
         camera.minimumExposure = -1
@@ -56,22 +116,18 @@ class ARViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     }
     
     func resetTracking() {
-        //virtualObjectInteraction.selectedObject = nil
-        
         let configuration = ARWorldTrackingConfiguration()
         configuration.planeDetection = [.horizontal, .vertical]
         if #available(iOS 12.0, *) {
             configuration.environmentTexturing = .automatic
         }
-        session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        session.run(configuration, options: [.resetTracking])
     }
     
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
         UIApplication.shared.isIdleTimerDisabled = true
-        resetTracking()
     }
     
     override func viewDidLoad() {
@@ -81,8 +137,7 @@ class ARViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         arView.session.delegate = self
 
         setupCamera()
-        //arView.scene.rootNode.addChildNode(focusSquare)
-        
+        resetTracking()
         arView.setupDirectionalLighting(queue: updateQueue)
     }
 }
