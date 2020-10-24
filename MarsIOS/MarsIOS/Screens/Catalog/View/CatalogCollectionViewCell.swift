a6911da938a81139dac4adf37109f548d4c7fc57
Andrey <andrewgurew123@gmail.com>
2020-10-24 13:25:43 +1000

    Добавлены базовые классы для каталога

---
+++ MarsIOS/MarsIOS/Screens/Catalog/View/CatalogCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CatalogCollectionViewCell.swift
+//  MarsIOS
+//
+//  Created by Andrey Gurev on 24.10.2020.
+//
+
+import UIKit
+
+final class CatalogCollectionViewCell: NSObject {
+    static let reuseIdentifier = "catalogCollectionViewCellReuseIdentifier"
+    
+    public func configure() {
+        
+    }
+}
