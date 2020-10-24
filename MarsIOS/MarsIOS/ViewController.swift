6e67737adeb5b9e8f90a653b00aa6032551d157b
Andrey <andrewgurew123@gmail.com>
2020-10-24 11:15:46 +1000

    Добавлен iOS проект

---
+++ MarsIOS/MarsIOS/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MarsIOS
+//
+//  Created by Andrey Gurev on 24.10.2020.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
